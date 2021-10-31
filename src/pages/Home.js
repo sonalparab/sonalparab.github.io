62bbd8613c60dc63423e7276c497dad485fa3360
ssp92 <ssp92@cornell.edu>
2021-10-30 23:46:33 -0400

    add basic homepage

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,10 +1,17 @@
 import React from 'react';
+import "./Home.css"
+import Headshot from "../images/Sonal.jpg";
+
 const Home = () => {
   return (
     <div>
-      <h3>Home</h3>
-      <div>
-        <p>Hello World its me</p>
+      <div className="container">
+        <img height={200} width={200} src={Headshot} align="left" />
+        <div className="text">
+          <p>My name is Sonal.</p>
+          <p>I am interested in software development.</p>
+          <p>The rest of this text is will be added later!</p>
+        </div>
       </div>
     </div>
   );
