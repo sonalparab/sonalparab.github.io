ade1db3d9105d849eb35e59a797fed3343c0f098
ssp92 <ssp92@cornell.edu>
2021-11-02 18:26:51 -0400

    hash router

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import './App.css';
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { HashRouter as Router, Switch, Route } from "react-router-dom";
 import Header from "./components/Header"
 import Home from "./pages/Home"
 import Projects from "./pages/Projects"
