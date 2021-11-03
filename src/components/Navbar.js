8e3c6e03e47cd32f9d22d623a92017b7f3493eb8
ssp92 <ssp92@cornell.edu>
2021-11-02 22:09:49 -0400

    highlight current tab

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,24 +1,63 @@
 import React from 'react';
-import { Link } from "react-router-dom";
+import { NavLink } from "react-router-dom";
 import "./Navbar.css"
+
+// {/* <script> */ }
+// const current = 0;
+// for (var i = 0; i < document.links.length; i++) {
+//   if (document.links[i].href === document.URL) {
+//     current = i;
+//   }
+// }
+// console.log(current);
+// document.links[current].className = 'current';
+//     </script >
+//     </>
 
 const Navbar = () => {
   return (
-    <ul class="nav">
+    <ul className="nav">
       <li>
-        <Link to="/">Home</Link>
+        <NavLink exact={true} to="/" activeStyle={{
+          fontWeight: "bold",
+          color: "#CD91FF"
+        }}>Home</NavLink>
       </li>
       <li>
-        <Link to="/projects">Projects</Link>
+        <NavLink to="/projects" activeStyle={{
+          fontWeight: "bold",
+          color: "#CD91FF"
+        }}>Projects</NavLink>
       </li>
       <li>
-        <Link to="/experience">Experience</Link>
+        <NavLink to="/experience" activeStyle={{
+          fontWeight: "bold",
+          color: "#CD91FF"
+        }}>Experience</NavLink>
       </li>
       <li>
-        <Link to="/interests">Interests</Link>
+        <NavLink to="/interests" activeStyle={{
+          fontWeight: "bold",
+          color: "#CD91FF"
+        }}>Interests</NavLink>
       </li>
-    </ul >
+    </ul>
   );
+
+  //   <ul className="nav">
+  //   <li>
+  //     <Link to="/">Home</Link>
+  //   </li>
+  //   <li>
+  //     <Link to="/projects">Projects</Link>
+  //   </li>
+  //   <li>
+  //     <Link to="/experience">Experience</Link>
+  //   </li>
+  //   <li>
+  //     <Link to="/interests">Interests</Link>
+  //   </li>
+  // </ul>
 }
 
 export default Navbar;
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -3,13 +3,13 @@ import "./Home.css"
 import Headshot from "../images/Sonal.jpg";
 
 const Home = () => {
+
   return (
     <div className="homeContainer">
       <img height={200} width={200} src={Headshot} align="left" className="headshot" />
       <div className="homeText">
         My name is Sonal.
-        <p>I am interested in software development. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. </p>
-        <p>The rest of this text is will be added later!</p>
+        <p>I am interested in software development. The rest of this text is will be added later!</p>
       </div>
     </div>
   );
